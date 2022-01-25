c2c56f3d7c3176b114432a577f853108176ee8eb
Valentin Borisenko <borison2004@yandex.ru>
2022-01-25 18:49:27 +0700

    resolve health task

--- src/js/__tests__/http.test.js
+++
@@ -1,9 +0,0 @@
-import fetchData from '../components/http';
-
-describe('fetchData function:', () => {
-  test.each(['url', 1, true, null, { url: 'http://localhost' }])('always return Err', () => {
-    expect(() => {
-      fetchData('url');
-    }).toThrow('Mock this!');
-  });
-});
--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,1 @@
 import health from './components/health';
-import sortByHealth from './components/sortByHealth';
-
-const newArr = sortByHealth([
-  { name: 'мечник', health: 10 },
-  { name: 'маг', health: 100 },
-  { name: 'лучник', health: 80 },
-]);
-
-console.log(newArr);

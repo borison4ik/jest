0dbef88b2e8c75d3ebb9f709482b9b23cea8cf12
Valentin Borisenko <borison2004@yandex.ru>
2022-01-25 15:09:57 +0700

    second task resolved

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import health from './components/health';
+import sortByHealth from './components/sortByHealth';
+
+const newArr = sortByHealth([
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+]);
+
+console.log(newArr);
